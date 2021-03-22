ec096787546f170b97c46dfb19736c0baf98311a
Moo <sosnsg@gmail.com>
2021-03-22 19:21:19 +0900

    Implement - lazyLoad.js

--- src/components/resultSection.js
+++ src/components/resultSection.js
@@ -1,26 +1,41 @@
+import lazyLoad from "../utils/lazyLoad.js";
+
 export default class ResultSection {
     constructor({ $target, data, onClick }) {
         this.data = data;
         this.onClick = onClick;
 
         this.$result = document.createElement("section");
-        this.$result.className = "result-section";
+        this.$result.className = "result-section";        
         $target.appendChild(this.$result);
 
         this.render();
+        lazyLoad();
     }
 
     setState(data) {
         this.data = data;
         this.render();
+        lazyLoad();
     }
 
     render() {
         if (this.data.length > 0) {
-            this.$result.innerHTML = this.data.map(image =>
-                `<article class="item" title=${image.name}><img src=${image.url} alt=${image.name} /></article>`
-            ).join("");
-            this.$result.querySelectorAll(".item").forEach((item, index) => {
+            this.$result.innerHTML = "";
+            this.data.map(datum => {
+                const imageWrapper = document.createElement("article");
+                imageWrapper.className = "image-wrapper";
+                imageWrapper.title = datum.name;
+
+                const image = document.createElement('img');
+                image.className = "lazy";
+                image.dataset.src = datum.url;
+                image.dataset.alt = datum.name;
+
+                imageWrapper.appendChild(image);
+                this.$result.appendChild(imageWrapper);
+            });
+            this.$result.querySelectorAll(".image-wrapper").forEach((item, index) => {
                 item.addEventListener("click", () => {
                     this.onClick(this.data[index]);
                 });

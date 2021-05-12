208645989e61680af3e323b4241361cd5f72603b
Moo <sosnsg@gmail.com>
2021-05-12 14:10:59 +0900

    Change - ways of handling errors

--- src/api.js
+++ src/api.js
@@ -1,35 +1,15 @@
-import errorHandler from "./utils/error-handler.js";
+import { errorHandler } from "./utils/error-handler.js";
 
 const API_ENDPOINT = "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
-const api = {
-    fetchCat: async (keyword) => {
-        const response = await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
-        if (response.ok) {
-            return response.json();
-        } else {
-            alert(errorHandler(response));
-            console.error(errorHandler(response));
-        }
-    },
-    fetchCatRandom: async () => {
-        const response = await fetch(`${API_ENDPOINT}/api/cats/random50`);
-        if (response.ok) {
-            return response.json();
-        } else {
-            alert(errorHandler(response));
-            console.error(errorHandler(response));
-        }
-    },
-    fetchCatDetails: async (id) => {
-        const response = await fetch(`${API_ENDPOINT}/api/cats/${id}`);
-        if (response.ok) {
-            return response.json();
-        } else {
-            alert(errorHandler(response));
-            console.error(errorHandler(response));
-        }
-    }
+export const api = {
+    fetchCat: async (keyword) => await fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`)
+        .then(respond => respond.json())
+        .catch(error => console.error(errorHandler(error))),
+    fetchCatRandom: async () => await fetch(`${API_ENDPOINT}/api/cats/random50`)
+        .then(respond => respond.json())
+        .catch(error => console.error(errorHandler(error))),
+    fetchCatDetails: async (id) => await fetch(`${API_ENDPOINT}/api/cats/${id}`)
+        .then(respond => respond.json())
+        .catch(error => console.error(errorHandler(error)))
 };
-
-export default api;

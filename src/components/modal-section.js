7a05903b248b9463c59627dc918c35f1de78941b
Moo <sosnsg@gmail.com>
2021-07-07 12:06:04 +0900

    Implement - fade effects

--- src/components/modal-section.js
+++ src/components/modal-section.js
@@ -10,37 +10,41 @@ export class ModalSection {
 
         document.body.addEventListener("keyup", (e) => {
             if (e.code == "Escape") {
-                this.$modal.style.display = "none";
+                this.$modal.querySelector("article").classList.remove("fade-in");
+                this.$modal.querySelector("article").classList.add("fade-out");
+                setTimeout(() => {
+                    this.$modal.style.display = "none";
+                }, 1000);
             }
         });
         document.body.addEventListener("click", (e) => {
             if (e.target.className === "modal-section" || e.target.className === "close") {
-                this.$modal.style.display = "none";
+                this.$modal.querySelector("article").classList.remove("fade-in");
+                this.$modal.querySelector("article").classList.add("fade-out");
+                setTimeout(() => {
+                    this.$modal.style.display = "none";
+                }, 1500);
             }
         });
     }
 
-    toggleDisplay(obj) {
-        if (obj.style.display === "none") {
-            obj.style.display = "block";
-        } else {
-            obj.style.display = "none";
-        }
-    }
-
     setState(data) {
         this.data = data;
-        this.toggleDisplay(this.$modal);
         this.render();
     }
 
     render() {
         const { name, url, temperament, origin } = this.data;
         const contentWrapper = document.createElement("article");
-        contentWrapper.className = "content-wrapper";
+        contentWrapper.className = "content-wrapper fade-in";
         contentWrapper.innerHTML += `<div class="title"><span>${name}</span><div class="close">x</div></div>`;
         contentWrapper.innerHTML += `<img src="${url}" alt="${name}"/>`;
         contentWrapper.innerHTML += `<div class="description"><span>성격: ${temperament}</span><br><span>태생: ${origin}</span></div>`;
-        this.$modal.appendChild(contentWrapper);
+        if (this.$modal.contains(document.querySelector(".content-wrapper"))) {
+            this.$modal.replaceChild(contentWrapper, this.$modal.querySelector(".content-wrapper"));
+        } else {
+            this.$modal.appendChild(contentWrapper);
+        }
+        this.$modal.style.display = "block"
     }
 }

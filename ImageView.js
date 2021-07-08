d04cfccb23132cdac4eebddea14f1a6f61599c34
555cider <sosng@gmail.com>
2021-07-08 15:06:36 +0900

    Implement - closing modal images

--- ImageView.js
+++ ImageView.js
@@ -1,11 +1,12 @@
-const IMAGE_PATH_PREFIX = 'https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public'
+const IMAGE_PATH_PREFIX =
+  "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public";
 
 export class ImageViewer {
   constructor({ $app, initialState }) {
     this.state = initialState;
 
-    this.$target = document.createElement('div');
-    this.$target.className = 'Modal ImageViewer';
+    this.$target = document.createElement("div");
+    this.$target.className = "Modal ImageViewer";
     $app.appendChild(this.$target);
   }
 
@@ -15,7 +16,16 @@ export class ImageViewer {
   }
 
   render() {
-    this.$target.innerHTML = `<div class="content">${this.state.filePath ? `<img src="${IMAGE_PATH_PREFIX}${this.state.filePath}">` : ''}</div>`;
-    this.$target.style.display = this.state.filePath ? 'block' : 'none';
+    this.$target.innerHTML = `<div class="content">${
+      this.state.filePath
+        ? `<img src="${IMAGE_PATH_PREFIX}${this.state.filePath}">`
+        : ""
+    }</div>`;
+    this.$target.style.display = this.state.filePath ? "block" : "none";
+    this.$target.addEventListener("click", function (e) {
+      const imageModal = document.querySelector(".ImageViewer");
+      imageModal.removeChild(imageModal.firstChild);
+      document.querySelector(".ImageViewer").style.display = "none";
+    });
   }
 }

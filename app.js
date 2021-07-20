dfd8257b67fe90ecc4ad22a921475e76630a88ba
Moo <sosnsg@gmail.com>
2021-07-20 14:09:56 +0900

    Remove - unused modules

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const indexRouter = require("./routes/index.js");
 
@@ -10,10 +8,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "pug");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);

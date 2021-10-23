dddcf32875af2d9d2edd9a7950053d86bde978aa
555cider <sosnsg@gmail.com>
2021-10-23 12:56:42 +0900

    Remove - unused static files

--- static/plugins/fullcalendar/plugin.js
+++
@@ -1,9 +0,0 @@
-require.config({
-    shim: {
-        'fullcalendar': ['moment', 'jquery'],
-    },
-    paths: {
-        'fullcalendar': 'static/plugins/fullcalendar/js/fullcalendar.min',
-        'moment': 'static/plugins/fullcalendar/js/moment.min',
-    }
-});
